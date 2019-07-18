55aa5950fce38bd7d4e5778a6e44b9dde3ecb41c
Nick Nikulin <n9179001111@gmail.com>
2019-07-18 22:54:58 +0300

    Day one warm up

---
+++ Day01/nested/main.go
@@ -0,0 +1,11 @@
+package main
+
+import {
+	"/Go18days/Day01/nested/hello/hello"
+	"/Users/nic/go/src/Go18days/Day01/nested/say/"
+	"fmt"
+}
+
+	fmt.Println("Start program...")
+	fmt.Println(say.CallFromSay() + hello.CallFromHello())
+
